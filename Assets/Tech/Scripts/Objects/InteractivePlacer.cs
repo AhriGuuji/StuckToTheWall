6ba19eab313f41e7dad3959a78378ebe04edb801
Lisa <i.m.lisaaa14@gmail.com>
2026-01-04 23:19:19 +0000

    Place geodo on floor implemented

--- Assets/Tech/Scripts/Objects/InteractivePlacer.cs
+++ Assets/Tech/Scripts/Objects/InteractivePlacer.cs
@@ -7,12 +7,25 @@ namespace Tech.Scripts.Objects
     {
         [Header("Additional Settings")] [SerializeField]
         private GameObject objectToPlace;
+        private Interactive _objectData;
         public bool IsPlaced {get; private set;}
+
+        private void Start()
+        {
+            _objectData = objectToPlace.GetComponent<Interactive>();
+        }
+
         public override void DoSomething()
         {
-            objectToPlace.transform.position = transform.position;
-            objectToPlace.SetActive(true);
-            IsPlaced = true;
+            if (_interactionManager.playerInventory.IsSelected(_objectData))
+            {
+                objectToPlace.transform.position = transform.position;
+                objectToPlace.SetActive(true);
+                IsPlaced = true;
+                _interactionManager.playerInventory.Remove(_objectData);
+            }
+            
+            //GetComponent<BoxCollider>().enabled = false;
         }
     }
 }
